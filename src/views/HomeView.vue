7b0b950a4d0c1d881df1cdb44ce3a89b2a68fed3
diniamalia21 <dinimalia21@gmail.com>
2022-10-20 15:25:06 +0700

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,43 @@
+<script setup></script>
+
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md text-center mt-4">
+                <div class="profile">
+                    <img src="../assets/profile_img.svg" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div claaa="col-md m-auto text-center mt-5">
+                <div class="description_about_me">
+                    <h4>Hello, i am Dini Amalia</h4>
+                    <p>
+                        I am 20 years old. I am a university student active on the Campus 
+                        of the Kosgoro Institute Business and Informatichs 1957
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container p-5 mt-5">
+        <div class="row">
+            <div class="col-md text-center">
+                <div class="card-body">
+                    <p>berikiut adalah portofolio yang saya buat</p>
+                    <a
+                    href="https://diniamalia.carrd.co/"
+                    class="btn btn-outline-primary"
+                    >click here!</a>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<style></style>
